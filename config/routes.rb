4c3af0bfd629d4d7e5bb05831b77528f456b4215
Barbara Fux <barbara.fux@gmx.at>
2019-12-26 18:26:35 +0100

    updated rails version

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
 
-  #root :to => 'landing#index'
-
-
   scope "(:locale)", :locale => /en|de|fr|it|es/ do
     root :to => 'landing#index'
     get "landing/index"
   end
-
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
